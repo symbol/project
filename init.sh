c87dd45e828420ae44231496dec996c16bc87564
wayonb <wayon@symbol.dev>
2023-11-09 22:24:21 -0500

    [monorepo] fix: recreate symbolic link on windows problem: the symbolic links for jenkins and linters are created as file links and not directory.          this is due to submodules are checkout last, so when symbolic links are created          the folders are not present in the submodules so they are created as files not directory. solution: recreate the jenkins and linters links after the submoule folders are present.

--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 set -ex
@@ -8,3 +7,11 @@ git -C _symbol config core.sparseCheckout true
 echo 'jenkins/*' >>.git/modules/_symbol/info/sparse-checkout
 echo 'linters/*' >>.git/modules/_symbol/info/sparse-checkout
 git submodule update --force --checkout _symbol
+
+# Windows recreate symlinks to jenkins and linters
+if [ "$(uname -o)" = "Msys" ]; then
+	echo "Recreating symlinks to jenkins and linters"
+	rm jenkins linters
+	ln -s _symbol/jenkins jenkins
+    ln -s _symbol/linters linters
+fi
