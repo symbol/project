342fbb4f7db78d5700564ff31b311ae0775acba0
wayonb <wayon@symbol.dev>
2023-11-17 14:00:05 -0500

    [monorepo] fix: git config fail on Jenkins in windows container

--- init.sh
+++ init.sh
@@ -3,10 +3,6 @@
 set -ex
 
 git submodule update --init
-git -C _symbol config core.sparseCheckout true
-echo 'jenkins/*' >>.git/modules/_symbol/info/sparse-checkout
-echo 'linters/*' >>.git/modules/_symbol/info/sparse-checkout
-git submodule update --force --checkout _symbol
 
 # Windows recreate symlinks to jenkins and linters
 if [ "$(uname -o)" = "Msys" ]; then
@@ -14,4 +10,9 @@ if [ "$(uname -o)" = "Msys" ]; then
 	rm -rf jenkins linters
 	ln -s _symbol/jenkins jenkins
 	ln -s _symbol/linters linters
+else
+	git -C _symbol config core.sparseCheckout true
+	echo 'jenkins/*' >>.git/modules/_symbol/info/sparse-checkout
+	echo 'linters/*' >>.git/modules/_symbol/info/sparse-checkout
+	git submodule update --force --checkout _symbol
 fi
